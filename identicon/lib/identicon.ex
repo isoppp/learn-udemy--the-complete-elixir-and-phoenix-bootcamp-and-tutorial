09880eef9512d1954ed32ff2de865a22fc393e6b
isoppp <prog@d2deck.com>
2019-11-10 12:30:26 -0800

    Add draw_image and save_image

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -6,10 +6,27 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn ({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
-    pixel_map = Enum.map grid, fn({_code, index}) ->
+    pixel_map = Enum.map grid, fn ({_code, index}) ->
       horizontal = rem(index, 5) * 50
       vertical = div(index, 5) * 50
       top_left = {horizontal, vertical}
@@ -22,7 +39,7 @@ defmodule Identicon do
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
-    grid = Enum.filter grid, fn({code, _index}) ->
+    grid = Enum.filter grid, fn ({code, _index}) ->
       rem(code, 2) == 0
     end
 
